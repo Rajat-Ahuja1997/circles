7df9411cc602752d9a58e97a4bb4313e0d71f026
Rajat Ahuja <rahuja445@gmail.com>
2023-04-01 00:28:54 -0400

    add typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CircleModule } from './circle/circle.module';
 
 @Module({
-  imports: [CircleModule],
+  imports: [
+    CircleModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+      host: 'localhost',
+      port: 5432,
+      username: 'rajatahuja',
+      database: 'rajatahuja',
+    }),
+  ],
 })
 export class AppModule {}
