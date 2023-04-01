e134c670f5d05067c77b88a0d91816f98ff2c194
Rajat Ahuja <rahuja445@gmail.com>
2023-04-01 00:41:31 -0400

    make posts subfolder

---
+++ src/post/post.entity.ts
@@ -0,0 +1,43 @@
+import { Column, Entity } from 'typeorm';
+import { Circle } from '../circle/circle.entity';
+
+// TypeORM entity for Post, it can be liked and replied to
+@Entity()
+export class Post {
+  // Post ID
+  @Column()
+  id: number;
+
+  @Column()
+  content: string;
+
+  @Column()
+  author: number;
+
+  @Column()
+  likes: number;
+
+  @Column()
+  created: Date;
+
+  @Column()
+  circle: Circle;
+}
+
+/**
+ * Possible tweet SQL table
+ * | Column Name   | Data Type  | Description                                      |
+|---------------|------------|--------------------------------------------------|
+| id            | integer    | Unique identifier for the tweet                  |
+| user_id       | integer    | Identifier for the user who posted the tweet     |
+| text          | text       | The content of the tweet                          |
+| created_at    | timestamp  | The date and time the tweet was posted            |
+| updated_at    | timestamp  | The date and time the tweet was last updated      |
+| retweet_count | integer    | The number of times the tweet has been retweeted   |
+| favorite_count| integer    | The number of times the tweet has been favorited   |
+| is_reply      | boolean    | Whether the tweet is a reply to another tweet     |
+| reply_to_tweet_id | integer | Identifier for the tweet that this tweet is a reply to |
+| hashtags      | text       | List of hashtags used in the tweet, separated by commas |
+| mentions      | text       | List of user IDs mentioned in the tweet, separated by commas |
+
+ */
